44127bf28aae09ecd80a95bd5567bd7f365e6a65
Samuel Lopez <samuello@ucm.es>
2020-05-01 21:00:13 +0200

    Spring Boot with Web server

---
+++ src/test/java/com/example/movieCatalog/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.movieCatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
